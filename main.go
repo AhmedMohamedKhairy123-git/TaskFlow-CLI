ee33000105431dc03d19cba9a6ba8743f4361882
ahmedmohamedkhairy123 <ahmedmohamedkhairy123@gmail.com>
2023-02-26 17:37:56 +0200

    Phase 2: Functions and control flow with menu system

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
-// Task represents a single task in our tracker
 type Task struct {
 	ID        int
 	Title     string
@@ -16,43 +18,121 @@ const (
 	appVersion = "1.0.0"
 )
 
+var tasks []Task
+var nextID int = 1
+var reader = bufio.NewReader(os.Stdin)
+
 func main() {
-	// Basic variables demonstration
-	var userName string
-	var userChoice int
+	showWelcome()
 	
-	// Constants and basic output
+	for {
+		showMenu()
+		choice := getUserChoice()
+		
+		if !processChoice(choice) {
+			break
+		}
+	}
+	
+	fmt.Println("Goodbye!")
+}
+
+func showWelcome() {
 	fmt.Printf("Welcome to %s v%s\n", appName, appVersion)
 	fmt.Println("================================")
+}
+
+func showMenu() {
+	fmt.Println("\n--- MENU ---")
+	fmt.Println("1. Add Task")
+	fmt.Println("2. List Tasks")
+	fmt.Println("3. Mark Task Complete")
+	fmt.Println("4. Exit")
+	fmt.Print("Enter choice: ")
+}
+
+func getUserChoice() int {
+	var choice int
+	fmt.Scanln(&choice)
+	return choice
+}
+
+func processChoice(choice int) bool {
+	switch choice {
+	case 1:
+		addTask()
+		return true
+	case 2:
+		listTasks()
+		return true
+	case 3:
+		markTaskComplete()
+		return true
+	case 4:
+		return false
+	default:
+		fmt.Println("Invalid choice. Please try again.")
+		return true
+	}
+}
+
+func addTask() {
+	fmt.Print("Enter task title: ")
+	title, _ := reader.ReadString('\n')
+	title = strings.TrimSpace(title)
 	
-	// Get user name
-	fmt.Print("Enter your name: ")
-	fmt.Scanln(&userName)
+	if title == "" {
+		fmt.Println("Task title cannot be empty!")
+		return
+	}
 	
-	// Create a simple task to demonstrate struct
-	task1 := Task{
-		ID:        1,
-		Title:     "Learn Go basics",
+	task := Task{
+		ID:        nextID,
+		Title:     title,
 		Completed: false,
 	}
 	
-	task2 := Task{2, "Complete Phase 1", false} // shorthand notation
+	tasks = append(tasks, task)
+	nextID++
 	
-	// Display welcome and tasks
-	fmt.Printf("\nHello, %s! Here are your initial tasks:\n\n", userName)
+	fmt.Printf("Task added with ID: %d\n", task.ID)
+}
+
+func listTasks() {
+	if len(tasks) == 0 {
+		fmt.Println("No tasks found.")
+		return
+	}
 	
-	// Manual task display (we'll improve this later)
-	fmt.Printf("Task #%d: %s - Completed: %v\n", task1.ID, task1.Title, task1.Completed)
-	fmt.Printf("Task #%d: %s - Completed: %v\n", task2.ID, task2.Title, task2.Completed)
+	fmt.Println("\n--- TASKS ---")
+	for _, task := range tasks {
+		status := " "
+		if task.Completed {
+			status = "✓"
+		}
+		fmt.Printf("[%s] %d: %s\n", status, task.ID, task.Title)
+	}
+}
+
+func markTaskComplete() {
+	if len(tasks) == 0 {
+		fmt.Println("No tasks to complete.")
+		return
+	}
 	
-	// Simple menu placeholder
-	fmt.Println("\n--- Menu Options (coming in Phase 2) ---")
-	fmt.Println("1. Add Task")
-	fmt.Println("2. List Tasks")
-	fmt.Println("3. Exit")
-	fmt.Print("\nEnter your choice (demo only, press any number): ")
-	fmt.Scanln(&userChoice)
+	listTasks()
 	
-	fmt.Printf("\nYou selected option %d. More features coming in Phase 2!\n", userChoice)
-	fmt.Println("Phase 1 complete! Ready for commit.")
+	fmt.Print("Enter task ID to mark complete: ")
+	var id int
+	fmt.Scanln(&id)
+	
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks[i].Completed = true
+			fmt.Printf("Task '%s' marked as complete!\n", task.Title)
+			return
+		}
+	}
+	
+	fmt.Printf("Task with ID %d not found.\n", id)
 }
